22cf411d4b3bcea6feb832264bdf8b9fabb4ce1c
Prasanna Achar <acharprasanna5@gmail.com>
2024-02-12 09:57:39 +0530

    New Commit

--- 07Router/src/components/Github/Github.jsx
+++ 07Router/src/components/Github/Github.jsx
@@ -1,1 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+
+function Github(){
+    const[data, setData] = useState("");
+    useEffect (() => {
+        fetch('https://api.github.com/users/prasanna-achar')
+        .then(res => res.json())
+        .then(data=> setData(data))
+    })
+    return (
+        <div className="text-center bg-gray-500 text-white p-4 
+        text-3xl">github followers: {data['followers']}
+        <img src={data['avatar_url']} alt="Git picture" />
+
+        </div>
+    )
+}
+
+export default Github
