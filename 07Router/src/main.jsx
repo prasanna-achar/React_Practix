26b74f28cdcca2047dda24615bee497958f726a7
Prasanna Achar <acharprasanna5@gmail.com>
2024-02-12 09:57:39 +0530

    New Commit

--- 07Router/src/main.jsx
+++ 07Router/src/main.jsx
@@ -7,6 +7,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
 
 const router = createBrowserRouter([
   {
@@ -27,6 +28,10 @@ const router = createBrowserRouter([
     {
       path:"user/:UserID",
       element:<User />
+    },
+    {
+      path:'/github',
+      element: <Github />
     }
   ]
   }  
