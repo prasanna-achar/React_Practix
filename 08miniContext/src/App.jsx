6d645de6841b81e7059c84d0821e769d05be4071
Prasanna Achar <acharprasanna5@gmail.com>
2024-02-12 20:40:26 +0530

    new commit

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
+import UserContext from './context/UserContext'
+import UserContextProvider from './context/UserContextProvider.jsx'
 
 import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
 
   return (
-    <>
-      <h1>Context API</h1>
-    </>
+    <UserContextProvider>
+      <h1> Hello world</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
