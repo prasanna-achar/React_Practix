e453e8cbf3587c93853ce457bc1b565276c1af1c
Prasanna Achar <acharprasanna5@gmail.com>
2024-02-19 15:26:16 +0530

    New Commit

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,54 @@
+import conf from '../conf/conf.js'
+import {Client, Account, ID} from 'appwrite'
+
+export class AuthService{
+    client = new Client;
+    account;
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+        this.account = new Account(client)
+    }
+    async createAccount({email, password, name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if(userAccount){
+                return this.loginAccount(email, password)
+                //call another account
+            }else{
+                return userAccount
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+    async loginAccount({email, password}){
+        try {
+            return await this.account.createEmailSession(email, password)
+        } catch (error) {
+            throw error;
+            
+        }
+    }
+    async getCurrentUser(){
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log("Get User Account:: ",error);
+        }
+        return null;
+    }
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions()
+        } catch (error) {
+            console.log("logout ::", error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService
